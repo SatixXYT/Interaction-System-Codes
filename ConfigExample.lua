60331b320388a0b9b91cf57225baf32a005c42e6
SatixXYT <108635481+SatixXYT@users.noreply.github.com>
2022-07-03 18:12:07 -0500

    Create ConfigExample.lua

---
+++ ConfigExample.lua
@@ -0,0 +1,13 @@
+local Interaccion = IS.new(Jugador, ParteDeInteractuado, {
+    UseRayCast = true;
+    OpenType = "Deafult";
+    KeyCode = "E";
+    XboxCode = "X";
+    MaxDistance = 7;
+    InteractOpen = "Distance";
+    UiStyle = "Deafult";
+    Axis = "Y";
+    IgnoreParts = {};
+    KeyCodeText = "kc";
+    Description = "Press kc to interact (td's)";
+})
