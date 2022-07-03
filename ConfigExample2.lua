de41bcaf0900d75a42ff6acec536a38e61ae00e2
SatixXYT <108635481+SatixXYT@users.noreply.github.com>
2022-07-03 18:26:30 -0500

    Create ConfigExample2.lua

---
+++ ConfigExample2.lua
@@ -0,0 +1,20 @@
+Interaccion:ChangeConfiguration(
+    {
+        BackgroundColor = Color3.fromRGB(255, 255, 255);
+        BackgroundTransparency = .6;
+        KeyColor = Color3.fromRGB(0, 0, 0);
+        BKeyTransparency = 0;
+        TitleColor = Color3.fromRGB(200, 200, 200);
+        DescriptionColor = Color3.fromRGB(255, 255, 255);
+        BKeyColor = Color3.fromRGB(255, 255, 255);
+        BTitleColor = Color3.fromRGB(200, 200, 200);
+        BDescriptionColor = Color3.fromRGB(255, 255, 255);
+        DescTransparency = 1;
+        TitleTransparency = 1;
+        Font = Enum.Font.SourceSansBold;
+        Rounded = true;
+        AlwaysOnTop = true;
+        ShowPosition = Vector3.new(0, 0, 0);
+        CornerRadius = .2;
+    }
+)
